be11bfff4da8646d2c91a40ae64266cb41582445
Anthony Gonsalves <anthony.gonsalves@westwing.de>
2020-05-08 14:58:15 +0200

    Binary tree implemented

---
+++ BinarySearchTree/BinarySearchTree/main.swift
@@ -0,0 +1,167 @@
+//
+//  main.swift
+//  BinarySearchTree
+//
+//  Created by Anthony Gonsalves on 25/04/20.
+//  Copyright © 2020 Anthony Gonsalves. All rights reserved.
+//
+
+import Foundation
+
+print("Demonstration of Binary search tree")
+
+class BinarySearchTree<Value: Comparable>: CustomStringConvertible {
+
+    class Node {
+        var value: Value
+        var left: Node?
+        var right: Node?
+
+        init(value: Value) {
+            self.value = value
+        }
+    }
+
+    var rootNode: Node?
+
+    func insert(value: Value) {
+        let node = insert(value: value, parentNode: rootNode)
+        print(node.value)
+    }
+
+    @discardableResult private
+    func insert(value: Value, parentNode: Node?) -> Node {
+        guard let aparentNode = parentNode else {
+            let node = Node(value: value)
+            if rootNode == nil { rootNode = node }
+            return node
+        }
+
+        switch value < aparentNode.value {
+        case true:
+            aparentNode.left = insert(value: value, parentNode: aparentNode.left)
+        case false:
+            aparentNode.right = insert(value: value, parentNode: aparentNode.right)
+        }
+        return aparentNode
+    }
+
+    let spaceBetweenDifferentLevels = 10
+    var description: String {
+        var toBeReturned = ""
+
+        func printOnConsole(parentNode: Node?, space: Int) {
+            guard let aparentNode = parentNode else {
+                return
+            }
+
+            let nextLevelspace = space + spaceBetweenDifferentLevels
+
+            printOnConsole(parentNode: aparentNode.left, space: nextLevelspace)
+
+            (0...nextLevelspace).forEach { _ in toBeReturned.append(" ") }
+            toBeReturned.append("\(aparentNode.value)\n")
+
+            printOnConsole(parentNode: aparentNode.right, space: nextLevelspace)
+        }
+
+        printOnConsole(parentNode: rootNode, space: 0)
+        return toBeReturned
+    }
+
+
+    func printOnConsole(parentNode: Node?, space: Int) {
+        guard let aparentNode = parentNode else {
+            return
+        }
+
+        let nextLevelspace = space + spaceBetweenDifferentLevels
+
+        printOnConsole(parentNode: aparentNode.left, space: nextLevelspace)
+
+        print("(\(aparentNode.value)")
+
+        printOnConsole(parentNode: aparentNode.right, space: nextLevelspace)
+
+    }
+
+
+
+
+    func search(value: Value) -> Node? {
+        return search(value: value, parentNode: rootNode)
+    }
+
+    private
+    func search(value: Value, parentNode: Node?) -> Node? {
+        guard let aparentNode = parentNode else { return nil }
+
+        if aparentNode.value == value {
+            return aparentNode
+        }
+
+        switch value < aparentNode.value {
+        case true:
+            return search(value: value, parentNode: aparentNode.left)
+        case false:
+            return search(value: value, parentNode: aparentNode.right)
+        }
+    }
+
+    func delete(value: Value, parentNode: Node?) -> Node? {
+        guard let aparentNode = parentNode else { return nil }
+        if aparentNode.value < value {
+            aparentNode.left = delete(value: value, parentNode: aparentNode.left)
+        } else if aparentNode.value > value {
+            aparentNode.right = delete(value: value, parentNode: aparentNode.right)
+        } else {
+            if aparentNode.left == nil, aparentNode.right == nil {
+                return nil
+            } else if let childLeftNode = aparentNode.left, aparentNode.right == nil {
+                aparentNode.value = childLeftNode.value
+                aparentNode.left = nil
+                return aparentNode
+            } else if let childRightNode = aparentNode.right, aparentNode.left == nil {
+                aparentNode.value = childRightNode.value
+                aparentNode.right = nil
+                return aparentNode
+            } else {
+                //Find the min in right sub tree
+                if let minRightNode = findMin(aparentNode.right) {
+                    aparentNode.value = minRightNode.value
+                    aparentNode.right = delete(value: minRightNode.value, parentNode: aparentNode.right)
+                    return aparentNode
+                }
+            }
+        }
+        return nil
+    }
+
+    func findMin(_ parentNode: Node?) -> Node? {
+        guard let aparentNode = parentNode else { return nil }
+        if aparentNode.left == nil {
+            return aparentNode
+        }
+        return findMin(aparentNode.left)
+    }
+}
+
+
+
+let bst = BinarySearchTree<Int>()
+
+let numbers = [25, 30, 29, 27]
+
+for number in numbers {
+    print("Inserting... \(number)")
+    bst.insert(value: number)
+}
+
+let exsistingNode = bst.search(value: 27)
+print("exsisting node \(exsistingNode)")
+
+let nonExsistingNode = bst.search(value: 26)
+print("exsisting node \(nonExsistingNode)")
+
+
+print(bst)
