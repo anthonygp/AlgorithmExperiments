65e21d4432012f4e05db6a923caec611cd7736ca
a.gonsalves <anthony.gonsalves@westwing.de>
2020-08-07 21:33:18 +0200

    Updated git ignore

---
+++ LowestCommonAncestor/LowestCommonAncestor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LowestCommonAncestor
+//
+//  Created by Anthony Gonsalves on 07.08.20.
+//  Copyright © 2020 Anthony Gonsalves. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
